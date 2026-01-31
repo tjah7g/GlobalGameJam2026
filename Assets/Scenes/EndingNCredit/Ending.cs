68b3e97e45840349b661b73e6c63a8aed256c8c2
vinnettayay <fanya.arthalita.fauzi@gmail.com>
2026-01-31 20:24:32 +0700

    SplashScreen

--- Assets/Scenes/EndingNCredit/Ending.cs
+++ Assets/Scenes/EndingNCredit/Ending.cs
@@ -5,13 +5,14 @@ using UnityEngine.SceneManagement;
 public class Ending : MonoBehaviour
 {
     [SerializeField] private string sceneName;
+    [SerializeField] private float time = 5;
     private void Start()
     {
         StartCoroutine(BackToMenu(sceneName));
     }
     private IEnumerator BackToMenu(string scene)
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(time);
         SceneManager.LoadScene(scene);
     }
 }
