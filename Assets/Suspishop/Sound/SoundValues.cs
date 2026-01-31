7c4555eef179f22836fdd33c104d6a6633ffccd6
Jeryan Eka Yuda <jejey09@gt.student.pens.ac.id>
2026-01-31 12:52:35 +0700

    Add Sound Manager

---
+++ Assets/Suspishop/Sound/SoundValues.cs
@@ -0,0 +1,13 @@
+//Author: Small Hedge Games
+//Updated: 13/06/2024
+
+namespace SmallHedge.SoundManager
+{
+    public enum SoundType
+    {
+        DoorSFX,
+        PotionSFX,
+        ServeOrderSFX,
+        CancelOrderSFX
+    }
+}
